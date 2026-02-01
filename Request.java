c8222d4159fbdf2bc4d47f96f6e9b96f19d36de2
sushantksah <sushantksah@gmail.com>
2026-02-01 12:59:54 -0500

    inital test

---
+++ Request.java
@@ -0,0 +1,18 @@
+import java.net.*;
+
+/**
+ * Inital Mapping
+ * 
+ * 
+ * 
+ */
+final class Request {
+    final static String CRLF = "\r\n";
+    final static int BUFFER_SIZE = 1024;
+
+    private Socket socket;
+    private String clientIP; 
+
+
+
+}
