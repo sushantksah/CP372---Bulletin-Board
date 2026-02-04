fd31c84e49d0531144968abd326b6c88eee788ec
sushantksah <sushantksah@gmail.com>
2026-02-04 14:27:23 -0500

    Uncs VERSION OF NOTE ! RIGHT? MAYBE! WRONG? PROBABLY

--- Note.java
+++ Note.java
@@ -3,13 +3,14 @@ import java.util.List;
 
 public class Note {
     public final int x, y;
-    public int noteWidth, noteHeight;
+    public final int noteWidth, noteHeight;
     public final String color;
     public final String message;
-    public  List<Pin> pins;
-    public boolean isPinned;
+    private List<Pin> pins;
+    private boolean isPinned;
 
-// i can't remember if note width and height are passed in here or not
+    public final int MAX_MESSAGE_LENGTH = 256;
+
     public Note(int x, int y, String color, String message, int noteWidth, int noteHeight) {
         this.x = x;
         this.y = y;
@@ -19,12 +20,28 @@ public class Note {
         this.message = message;
         this.pins = new ArrayList<>();
         this.isPinned = false;
+
     }
 
-    // Containment, ensuring that the points lie within the note rectangle 
-    // pins cannot be on the edge of the note
-    public boolean contains(int pinX, int pinY) {
-        return this.x < pinX &&  pinX < (this.noteWidth + this.x) && this.y < pinY &&  pinY < (noteHeight + this.y);
+    public String getColor() {
+        return this.color;
+    }
+
+    public String getMessage() {
+        return this.message;
+    }
+
+    public List<Pin> getPins() {
+        return this.pins;
+    }
+
+    public boolean getPinnedStatus() {
+        return this.isPinned;
+    }
+
+    // Setters
+    public void setPinStatus(boolean bool) {
+        this.isPinned = bool;
     }
 
     public void addPin(Pin pin) {
@@ -35,4 +52,33 @@ public class Note {
             this.isPinned = true;
         }
     }
+
+    public void removePin(Pin pin) {
+        this.pins.remove(pin);
+
+        // no longer 0 pins
+        if (this.pins.size() == 0) {
+            this.isPinned = false;
+        }
+    }
+
+    // Containment, ensuring that the points lie within the note rectangle
+    // pins cannot be on the edge of the note
+    public boolean containsPoint(int pointX, int pointY) {
+        return this.x < pointX && pointX < (this.noteWidth + this.x) && this.y < pointY
+                && pointY < (noteHeight + this.y);
+    }
+
 }
+ 
+
+    
+
+    
+    
+
+    
+        
+    
+
+ 
