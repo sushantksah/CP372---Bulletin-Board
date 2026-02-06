7a992bf68354454c07cb8665075d27fcc26a9bf5
Jessica Tidd <jessicasydtidd@gmail.com>
2026-02-06 18:05:58 -0500

    pin comments

--- Server/Pin.java
+++ Server/Pin.java
@@ -1,18 +1,27 @@
 package Server;
 import java.util.Objects;
 
+/*
+Pin class is the object that contains the pin and the coordinates of the pin.
+Manages pin coordinates and equality.
+*/
+
 public class Pin {
     private final int x;
     private final int y;
 
+    // Initializes the pin and its coordinates
     public Pin(int x, int y) {
         this.x = x;
         this.y = y;
     }
 
+    /* Getters */
     public int getX() { return x; }
     public int getY() { return y; }
 
+    /* Overrides */
+    // Checks if two pins are equal if they have the same coordinates
     @Override
     public boolean equals(Object o) {
         if (!(o instanceof Pin)) return false;
@@ -20,6 +29,7 @@ public class Pin {
         return x == other.x && y == other.y;
     }
 
+    // Returns the hash code of the pin
     @Override
     public int hashCode() {
         return Objects.hash(x, y);
