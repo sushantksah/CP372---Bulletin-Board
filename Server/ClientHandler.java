9922b29b3aea363603d61d408cfd01037ecf79b1
Jessica Tidd <jessicasydtidd@gmail.com>
2026-02-06 18:19:26 -0500

    ch comments

--- Server/ClientHandler.java
+++ Server/ClientHandler.java
@@ -3,18 +3,25 @@ import java.io.*;
 import java.net.*;
 import java.util.*;
 
-// Client Handler Class 
+/*
+ClientHandler class is the object that handles the client connection.
+Manages the client connection, reads the request, and sends the response.
+*/
+
 public class ClientHandler implements Runnable {
     private final Socket socket;
     private final Board board;
 
+    // Initializes the client handler and its socket and board
     public ClientHandler(Socket socket, Board board) {
         this.socket = socket;
         this.board = board;
     }
 
+    // Runs the client handler
     @Override
     public void run() {
+        // Try to get I/O streams from the socket
         try (
             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             PrintWriter out = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()), true)
